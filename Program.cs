3be4c601ce65d9c8e4c6b11cac0b2ec9f17fdf06
travjsmit <inivid@icloud.com>
2024-07-27 11:03:48 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ListsDataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int[] arr = { 54, 41, 87, 65, 45 };
+
+            arr.app
+        }
+    }
+}
