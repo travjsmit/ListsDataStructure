0374089191e2b6d8a54e9c18521d92d62af782ac
travjsmit <inivid@icloud.com>
2024-07-27 13:38:07 -0700

    Removed array. Created dynamic array / array list. Created MyArrayListTests and multiple tests.

---
+++ MyArrayList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListsDataStructure
+{
+    public class MyArrayList
+    {
+        public int[] list;
+        int size = 0;
+
+        public MyArrayList()
+        {
+            list = new int[10];
+        }
+
+         public MyArrayList(int size)
+        {
+            list = new int[size];
+        }
+
+        /// <summary>
+        /// Will add the incoming value to the end of the list.
+        /// </summary>
+        public void Append(int value)
+        {
+            list[size++] = value;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@
         {
             Console.WriteLine("Hello, World!");
 
-            int[] arr = { 54, 43, 41, 87, 65, 45, 3, 2, 1, 90, 43, 43};
+            int[] arr = new int[10];
 
             List<int> arr1 = arr.Append(2).ToList ();
 
             string[] arrString = { "Hello", "World", "Hi" };
+
+            
 
             arr1.Add(2);
             PrintArray(arr);
