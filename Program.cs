95b2fe2469055c568f4853c7762266d4634bdb9a
travjsmit <inivid@icloud.com>
2024-07-27 11:29:27 -0700

    Routine Commit.

--- Program.cs
+++ Program.cs
@@ -6,9 +6,25 @@
         {
             Console.WriteLine("Hello, World!");
 
-            int[] arr = { 54, 41, 87, 65, 45 };
+            int[] arr = { 54, 41, 87, 65, 45, 3, 2, 1, 90, 43};
 
-            arr.app
+            IEnumerable<int> arr1 = arr.Append(2);
+
+            string[] arrString = { "Hello", "World", "Hi" };
+
+            arr1.Append(2);
+            PrintArray(arr);
+            PrintArray(arrString);
+            PrintArray(arr1.ToArray());
+
+        }
+        public static void PrintArray<T>(T[] arr)
+        {
+            foreach(T i in arr)
+            { 
+                Console.WriteLine(i + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
