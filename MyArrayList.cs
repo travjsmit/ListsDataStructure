f34bc9eff497aa07b8d19e9e6e6ac6a4783f58a1
travjsmit <inivid@icloud.com>
2024-07-27 15:05:43 -0700

    Added Start and Tests for Start.

--- MyArrayList.cs
+++ MyArrayList.cs
@@ -18,7 +18,7 @@ namespace ListsDataStructure
             _list = new int[10];
         }
 
-         public MyArrayList(int size)
+        public MyArrayList(int size)
         {
             _list = new int[size];
         }
@@ -32,17 +32,29 @@ namespace ListsDataStructure
             _list[_size++] = value;
         }
 
+        public void AddStart(int value)
+        {
+            IncreaseCapacity();
+            for(int i = _size; i >= 0; i--)
+            {
+                _list[i + 1] = i;
+            }
+
+            _list[0] = value;
+            _size++;
+        }
+
         private void IncreaseCapacity()
         {
             if (_size == _list.Length) return;  
-            int[] newArray = new int[_size * 2];
-            for(int i = 0; i < _size; i++)
+            int[] newArray = new int[_size * 2]; // Creates new array as dynamic list changes
+            for (int i = 0; i < _size; i++)
             {
                 newArray[i] = _list[i]; 
             }   
             
             _list = newArray;           
-            }
         }
+        
     }
 }
