75be81eb9ea5cd4c322f27a35f7a31a03459d6bf
travjsmit <inivid@icloud.com>
2024-07-30 15:38:37 -0700

    Classwork 7/30; added test methods lines 81-173 and in MyArrayList added lines 52-98.

--- ListsDataStructure/MyArrayList.cs
+++ ListsDataStructure/MyArrayList.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -31,29 +32,70 @@ namespace ListsDataStructure
             IncreaseCapacity();
             _list[_size++] = value;
         }
-
+        /// <summary>
+        /// Will add the incoming value to the start of the list
+        /// and will shift all remaining values to the right 1 position
+        /// </summary>
+        /// <param name="value"></param>
         public void AddStart(int value)
         {
             IncreaseCapacity();
             for(int i = _size; i >= 0; i--)
             {
-                _list[i + 1] = i;
+                _list[i + 1] = _list[i];
             }
 
             _list[0] = value;
             _size++;
         }
 
-        private void IncreaseCapacity()
+        public void IncreaseCapacity()
         {
-            if (_size == _list.Length) return;  
-            int[] newArray = new int[_size * 2]; // Creates new array as dynamic list changes
+            if (_size != _list.Length) return;
+            int[] newArray = new int[_size * 2];
             for (int i = 0; i < _size; i++)
             {
-                newArray[i] = _list[i]; 
-            }   
-            
-            _list = newArray;           
+                newArray[i] = _list[i];
+            }
+            _list = newArray;
+        }
+
+        public void DeleteStart()
+        {
+            DeleteFromIndex(0);
+        }
+
+        public void DeleteEnd()
+        {
+            if (_size <= 0) return;
+            _list[--_size] = 0;
+        }
+
+        public void DeleteFromIndex(int index)
+        {
+            if (_size <= 0) return;
+            for (int i = index; i < _size; i++)
+            {
+                if (i + 1 == _size)
+                {
+                    _list[i] = 0;
+                    break;
+                }
+                _list[i] = _list[i + 1];
+            }
+            --_size;
+        }
+
+        public void Insert(int value, int indexToInswerAt)
+        {
+            IncreaseCapacity();
+            for( int i = _size; i < indexToInswerAt; i--)
+            {
+                _list[i + 1] = _list[i];
+            }
+
+            _list[0] = value;
+            _size++;    
         }
         
     }
--- ListsDataStructureTests/MyArrayListTests.cs
+++ ListsDataStructureTests/MyArrayListTests.cs
@@ -78,7 +78,99 @@ namespace ListsDataStructure.Tests
             //Act
             list.AddStart(4);
             //Assert
-            Assert.AreEqual(list.List[0], 4);
+            Assert.AreEqual(4, list.List[0]);
+            Assert.AreEqual(10, list.List[1]);
+        }
+
+        [TestMethod()]
+        public void Insert_ListIsEmpty_ShouldAddToStart()
+        {
+            // Arrange
+            MyArrayList list = new MyArrayList();
+            // Act
+            list.Insert(5, 0);
+            // Assert
+            Assert.AreEqual(5, list.List[0]);
+        }
+
+        [TestMethod()]
+        public void Insert_ListIsEmptyIndexIsInvalid_ShouldThrowException()
+        {
+            // Arrange
+            MyArrayList list = new MyArrayList();
+            // Act
+
+            // Assert
+            Assert.ThrowsException<IndexOutOfRangeException>(
+                () => list.Insert(5, 2),
+                "Indext is invalid!");
+        }
+
+        [TestMethod()]
+        public void Insert_ListHasValues_ShouldInsertAtCorrectPosition()
+        {
+            // Arrange
+            MyArrayList list = new MyArrayList();
+            list.Append(5);
+            list.Append(3);
+            list.Append(2);
+            // Act
+            list.Insert(8, 1);
+            // Assert
+            Assert.AreEqual(5, list.List[0]);
+            Assert.AreEqual(8, list.List[1]);
+            Assert.AreEqual(3, list.List[2]);
+        }
+
+        [TestMethod()]
+        public void DeleteStart_ListHasValues_ShouldDeleteFirstElement()
+        {
+            // Arrange
+            MyArrayList list = new MyArrayList();
+            list.Append(5);
+            list.Append(3);
+            list.Append(2);
+            // Act
+            list.DeleteStart();
+            //Assert
+            Assert.AreEqual(3, list.List[0]);
+            Assert.AreEqual(2, list.List[1]);
+            Assert.AreEqual(0, list.List[2]);
+            Assert.IsTrue(list.Size == 2);
+        }
+
+        [TestMethod()]
+        public void DeleteStart_ListIsFull_ShouldDeleteFirstElement()
+        {
+            // Arrange
+            MyArrayList list = new MyArrayList(3);
+            list.Append(5);
+            list.Append(3);
+            list.Append(2);
+            //Act
+            list.DeleteStart();
+            // Assert
+            Assert.AreEqual(3, list.List[0]);
+            Assert.AreEqual(2, list.List[1]);
+            Assert.AreEqual(0, list.List[2]);
+            Assert.IsTrue(list.Size == 2);
+        }
+
+        [TestMethod()]
+        public void DeleteEnd_ListHasValues_ShouldDeleteFromEnd()
+        {
+            // Arrange
+            MyArrayList list = new MyArrayList();
+            list.Append(5);
+            list.Append(3);
+            list.Append(2);
+            //Act
+            list.DeleteEnd();
+            //Assert
+            Assert.IsTrue(list.Size == 2);
+            Assert.AreEqual(5, list.List[0]);
+            Assert.AreEqual(3, list.List[1]);
+            Assert.AreEqual(0, list.List[2]);
         }
     }
 }
